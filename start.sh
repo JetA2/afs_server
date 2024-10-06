cfe9ca6b3109a570e0e781a31dc1b4c568c87613
Peter Lundkvist <peter.lundkvist@applicate.info>
2024-10-06 22:33:44 +0200

    Add comment

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 /usr/bin/python /root/afs/afs_server/afs_server.py &
+
+# Remove below line if not using floppy_sound (https://github.com/JetA2/floppy_sound)
 /usr/bin/floppy_sound 2 1024 /root/afs/head_step.wav &
